9c3372f346dda62a0765f8258540340f0e5ea116
С - 1 < - 1@DESKTOP-C3171P9>
2024-06-12 18:30:20 +0500

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <Windows.h>
+#include <string.h>
+using namespace std;
+
+class MyString 
+{
+public:
+
+	MyString() 
+	{
+		size = 80;
+		char* word = new char[size];
+	
+	}
+
+	MyString(int size)
+	{
+	
+		int newSize = size;
+		char* word = new char[newSize];
+		count++;
+
+	}
+
+	MyString(const char* word)
+	{
+		size = strlen(word)+1;
+		this->word = new char [size] {0};
+		memcpy(this->word, word, size);
+		count++;
+	}
+
+	void operator = (const MyString& other)
+	{
+		this->word = other.word;
+	}
+
+
+
+
+
+
+
+	~MyString()
+	{
+		
+		delete[] word;
+		count--;
+	}
+
+
+
+
+private:
+	
+	char* word;
+	int size;
+	static size_t count;
+
+};
+
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	MyString one, two("Hello");
+	
+
+
+
+
+	return 0;
+}
